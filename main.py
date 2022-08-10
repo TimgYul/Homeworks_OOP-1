b53f6805251da2cded770b51d2e7cb554f18745f
Yuliya Timganova <yulchik12-12y@yandex.ru>
2022-08-10 18:26:22 +0500

    Initial

---
+++ main.py
@@ -0,0 +1,7 @@
+class students:
+    def _init_(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+
